8cedc7108802037a90b24a1a8f82e5d7a349813c
smrbts <samueljustinroberts@gmail.com>
2019-04-25 10:01:51 -0500

    Done.

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,3 +1,6 @@
+#Historical Figures
 class Figure < ActiveRecord::Base
-  # add relationships here
+  has_many :landmarks
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
 end
