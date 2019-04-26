31e6c4ebb72ffe12c0f4099c8de94bb49244ce38
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-25 21:46:22 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,28 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+  get '/landmarks/:id' do
+    @landmark = current_landmark
+    erb :'landmarks/show'
+  end
+  get '/landmarks/:id/edit' do
+    @landmark = current_landmark
+    erb :'landmarks/edit'
+  end
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect :"landmarks/#{@landmark.id}"
+  end
+  patch '/landmarks/:id' do
+    current_landmark.update(params[:landmark])
+    redirect :"landmarks/#{current_landmark.id}"
+  end
+  def current_landmark
+    Landmark.find(params[:id])
+  end
 end
